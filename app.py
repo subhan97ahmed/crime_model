1d1a2fb00381180a6b2a491bdd77ca8c73ab59dd
subhan <subhan97ahmed@gmail.com>
2021-08-11 04:41:47 +0500

    fixed Error R10 (Boot timeout) of heroku 2

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import uvicorn as uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 import numpy  as np
+
+
 import new_model
 from Crime import Crime, Csv_Data
 import pickle
@@ -63,25 +65,25 @@ def get_name(name: str):
     return {"message": "hello " + name}
 
 
-# @app.post('/predict')
-# def predict_rate(data: Crime):
-#     data = data.dict()
-#     print(data)
-#     year = data['year']
-#     month = data['month']
-#     area1 = data['area1']
-#     area2 = data['area2']
-#     crimeType = data['crimeType']
-#     pre = model.predict([[year, month, area1, area2, crimeType]])
-#     return {
-#         'year': str(year),
-#         'month': str(month),
-#         'area1': str(area1),
-#         'area2': str(area2),
-#         'crimeType': str(crimeType),
-#         'prediction': str(pre),
-#     }
-#
+@app.post('/predict')
+def predict_rate(data: Crime):
+    data = data.dict()
+    print(data)
+    year = data['year']
+    month = data['month']
+    area1 = data['area1']
+    area2 = data['area2']
+    crimeType = data['crimeType']
+    pre = model.predict([[year, month, area1, area2, crimeType]])
+    return {
+        'year': str(year),
+        'month': str(month),
+        'area1': str(area1),
+        'area2': str(area2),
+        'crimeType': str(crimeType),
+        'prediction': str(pre),
+    }
+
 
 @app.post('/predicts')
 def predict_rate_of_different_districts(data: Crime_Wo_Districts):
