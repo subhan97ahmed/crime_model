322041a7a739e0c63b5e77ebc2a24855b1c310d9
subhan <subhan97ahmed@gmail.com>
2021-08-11 20:22:09 +0500

    optimized new_model.py

--- new_model.py
+++ new_model.py
@@ -1,15 +1,14 @@
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
-import pandas as pd
+from pandas import DataFrame
 import pickle
 def new_model(csv):
-    df= pd.DataFrame(csv, columns=['Year', 'Month', 'Lat', 'Log', 'Crime Type', 'Reported Number'])
-    X = df.drop('Reported Number', axis=1)
-    y = np.asarray(df['Reported Number'])
+    df= DataFrame(csv, columns=['Year', 'Month', 'Lat', 'Log', 'Crime Type', 'Reported Number'])
+    X,y = df.drop('Reported Number', axis=1), np.asarray(df['Reported Number'])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
     clf = MLPRegressor(solver='lbfgs', activation='relu', hidden_layer_sizes=(15, 2, 35, 62, 9, 45, 8, 2),
-                       random_state=1, max_iter=5000, )
+                       random_state=1, max_iter=2000, )
     clf.fit(X_train, y_train)
     acc = clf.score(X_test, y_test)
     out = open("user_model.pkl","wb")
